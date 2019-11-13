d2ea65307998e0dadbb066d98b7ecc0accf47ed3
wernerjoss <werner@hoernerfranzracing.de>
2019-11-13 11:35:50 +0100

    impoved count method

--- classes/ipcount.class.php
+++ classes/ipcount.class.php
@@ -10,16 +10,19 @@ class ipCount {
 	
 	function count($cache, $ip) {
 		// see https://discourse.getgrav.org/t/best-practice-for-saving-managing-data-in-plugin/407/4
-		$file = File::instance(DATA_DIR . 'counter/counter.txt');
-		if(!isset($_SESSION['counter'])) { // It's the first visit in this session, so count us as new visitor
+		session_start(); // Should always be on top
+		if(!isset($_SESSION['counter'])) { // It's the first visit in this session
+			$file = File::instance(DATA_DIR . 'counter/counter.txt');
 			$counter = (int) $file->load();
 			$counter++;
 			$file->save($counter);
 			$_SESSION['counter'] = $counter;
-			//set the counter vars
-			$this->counter = $counter;
-			return $this->counter;
-		}
+			
+		} else { // It's not the first time, do not update the counter
+	        $counter = $_SESSION['counter'];
+        }
+		$this->counter = $counter;
+		return $this->counter;
 	}
 }
 ?>
